7530618db5719cd1e86263327b12190035c97cb8
TrungNT <nickartin13@gmail.com>
2016-04-02 15:35:50 +0300

    Add libcheck

---
+++ libcheck/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# cd <dir>
+# mkdir Build
+# cd Build
+# # you can pass -DCMAKE_INSTALL_PREFIX=/path/to/somewhere to install to an alternate location
+# cmake .. -DCMAKE_BUILD_TYPE=Release # or Debug if you are investigating a crash
+# make
+# make install
+# cd ..
+
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.local/lib64/
+# export LIBRARY_PATH=$LIBRARY_PATH:~/.local/lib64/
+# export CPATH=$CPATH:~/.local/include
+# export LIBRARY_PATH=$LIBRARY_PATH:~/.local/lib
+# export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.local/lib
+
+INSTALL_DIR=$PREFIX
+CONDA_ENV_PATH=$(conda info -e | grep '*' | tr -s ' ' | cut -d' ' -f3)
+
+####### for mac ######
+if [ `uname` == Darwin ]; then
+    echo "Darwin detected!"
+    export CC=clang
+    export CXX=clang++
+####### for linux #######
+else
+    echo "Linux detected!"
+    export CC=gcc
+    export CXX=g++
+fi
+
+./configure --prefix=$INSTALL_DIR --libdir=$CONDA_ENV_PATH/lib  --includedir=$CONDA_ENV_PATH/include
+make
+# ignore the tests
+# make check
+make install
