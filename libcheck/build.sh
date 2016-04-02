b40d46b7113301f8031507e4a69f1feda097d896
TrungNT <nickartin13@gmail.com>
2016-04-02 16:47:38 +0300

    fix libcheck;pygpu

--- libcheck/build.sh
+++ libcheck/build.sh
@@ -28,12 +28,12 @@ else
     export CC=gcc
     export CXX=g++
 
-    # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_ENV_PATH/lib
-    # export LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_ENV_PATH/lib
-    # export CPATH=$LD_LIBRARY_PATH:$CONDA_ENV_PATH/include
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_ENV_PATH/lib
+    export LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_ENV_PATH/lib
+    export CPATH=$LD_LIBRARY_PATH:$CONDA_ENV_PATH/include
 fi
 
-./configure --prefix=$INSTALL_DIR --bindir=$CONDA_ENV_PATH/bin --libdir=$CONDA_ENV_PATH/lib --includedir=$CONDA_ENV_PATH/include
+./configure --prefix=$INSTALL_DIR --bindir=$CONDA_ENV_PATH/bin
 
 make
 # ignore the tests
