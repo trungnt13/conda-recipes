fd28018c9aaa0c74a0e12cdd8515ba4d8bfbb748
TrungNT <nickartin13@gmail.com>
2016-04-03 15:27:14 +0300

    fix bug with post-link pygpu

--- pygpu/post-link.sh
+++ pygpu/post-link.sh
@@ -2,17 +2,15 @@
 
 mkdir -p $PREFIX/etc/conda/deactivate.d
 mkdir -p $PREFIX/etc/conda/activate.d
+
 # deactivate.d
-cat <<EOF > $PREFIX/etc/conda/deactivate.d/openmpi.sh
-#!/bin/sh
-unset CPATH
+cat <<"EOF" > $PREFIX/etc/conda/deactivate.d/pygpu.sh
 export CPATH=$CPATH_OLD
-unset CPATH_OLD
+export CPATH_OLD=
 EOF
 
 # activate.d
-cat <<EOF > $PREFIX/etc/conda/activate.d/openmpi.sh
-#!/bin/sh
+cat <<"EOF" > $PREFIX/etc/conda/activate.d/pygpu.sh
 export CPATH_OLD=$CPATH
-export CPATH=$CPATH:$PREFIX/include
+export CPATH=$CPATH:$CONDA_ENV_PATH/include
 EOF
