e4c7b959d9027967fb9c9e7b508e6697751abbd2
TrungNT <nickartin13@gmail.com>
2016-07-15 19:42:07 +0300

    update pygpu

--- pygpu/build.sh
+++ pygpu/build.sh
@@ -2,11 +2,14 @@
 # If conda cannot find gpuarray/array.h :
 # export CPATH=$CPATH:/path/to/conda/envs/name/include
 
-
+# Build ...
 BUILD_DIR=$SRC_DIR/Build
-INSTALL_DIR=$PREFIX
+INSTALL_DIR=~/.local/libgpuarray
+# $PREFIX/libgpuarray
 
 mkdir $BUILD_DIR
+rm -rf $INSTALL_DIR # remove old libgpuarray
+mkdir $INSTALL_DIR
 cd $BUILD_DIR
 
 ####### for mac ######
@@ -35,3 +38,35 @@ python setup.py build_ext -L $INSTALL_DIR/lib -I $INSTALL_DIR/include
 python setup.py install
 
 rm -rf $BUILD_DIR
+
+# Add PATH to .bashrc
+if [ `uname` == Darwin ]; then
+    if ! grep -Fxq 'export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:~/.local/libgpuarray/lib' ~/.bashrc
+        then
+            echo "" >> ~/.bashrc
+            echo '################# libgpuarray' >> ~/.bashrc
+            echo 'export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:~/.local/libgpuarray/lib' >> ~/.bashrc
+            export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:~/.local/libgpuarray/lib
+        fi
+else
+    echo "" >> ~/.bashrc
+    echo '################# libgpuarray' >> ~/.bashrc
+fi
+
+if ! grep -Fxq 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.local/libgpuarray/lib' ~/.bashrc
+    then
+        echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.local/libgpuarray/lib' >> ~/.bashrc
+        export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.local/libgpuarray/lib
+    fi
+
+if ! grep -Fxq 'export LIBRARY_PATH=$LIBRARY_PATH:~/.local/libgpuarray/lib' ~/.bashrc
+    then
+        echo 'export LIBRARY_PATH=$LIBRARY_PATH:~/.local/libgpuarray/lib' >> ~/.bashrc
+        export LIBRARY_PATH=$LIBRARY_PATH:~/.local/libgpuarray/lib
+    fi
+
+if ! grep -Fxq 'export CPATH=$CPATH:~/.local/libgpuarray/include' ~/.bashrc
+    then
+        echo 'export CPATH=$CPATH:~/.local/libgpuarray/include' >> ~/.bashrc
+        export CPATH=$CPATH:~/.local/libgpuarray/include
+    fi
--- pygpu/pre-unlink.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-rm -f $PREFIX/etc/conda/deactivate.d/pygpu.sh
-rm -f $PREFIX/etc/conda/activate.d/pygpu.sh
